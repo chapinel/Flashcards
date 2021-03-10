dee51ccc265565903c2f132f7acdd33232ff6b28
Elizabeth Chapin <chapinel@oregonstate.edu>
2021-03-10 09:06:54 -0800

    Added an additional screen and functionality to edit starting flashcard or add a new flashcard

---
+++ Flashcards/CreationViewController.swift
@@ -0,0 +1,80 @@
+//
+//  CreationViewController.swift
+//  Flashcards
+//
+//  Created by Elizabeth Bakewell on 3/6/21.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+    
+    var flashcardsController: ViewController!
+
+    @IBOutlet weak var questionTextField: UITextField!
+    
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    @IBOutlet weak var topChoiceTextField: UITextField!
+    
+    @IBOutlet weak var middleChoiceTextField: UITextField!
+    
+    @IBOutlet weak var bottomChoiceTextField: UITextField!
+    
+    var initialQuestion: String?
+    var initialAnswer: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        questionTextField.text = initialQuestion
+        answerTextField.text = initialAnswer
+    }
+    
+
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        let questionText = questionTextField.text
+        
+        let answerText = answerTextField.text
+        
+        let choiceTop = topChoiceTextField.text
+        
+        let choiceMiddle = middleChoiceTextField.text
+        
+        let choiceBottom = bottomChoiceTextField.text
+        
+        if questionText == nil || questionText!.isEmpty || answerText == nil || answerText!.isEmpty || choiceTop == nil || choiceTop!.isEmpty || choiceMiddle == nil || choiceMiddle!.isEmpty || choiceBottom == nil || choiceBottom!.isEmpty {
+            let okAction = UIAlertAction(title: "Go Back", style: .default)
+            
+            let alert = UIAlertController(title: "Oops! Something's missing...", message: "You need to complete all fields before you can finish making your card.", preferredStyle: .alert)
+            
+            alert.addAction(okAction)
+            
+            present(alert, animated: true)
+        }
+        
+        else {
+            
+            flashcardsController.updateFlashcard(question: questionText!, answer: answerText!, topChoice: choiceTop!, middleChoice: choiceMiddle!, bottomChoice: choiceBottom!)
+            
+            dismiss(animated: true)
+            
+        }
+        
+    
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -43,6 +43,7 @@ class ViewController: UIViewController {
         answerBottom.layer.borderWidth = 3.0
         answerBottom.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
     }
+    
 
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         if questionCard.isHidden {
@@ -63,6 +64,28 @@ class ViewController: UIViewController {
     }
     @IBAction func didTapOptionThree(_ sender: Any) {
         answerBottom.isHidden = true
+    }
+    
+    func updateFlashcard(question: String, answer: String, topChoice: String, middleChoice: String, bottomChoice: String) {
+        questionCard.text = question
+        answerCard.text = answer
+        
+        answerTop.setTitle(topChoice, for: .normal)
+        answerMiddle.setTitle(middleChoice, for: .normal)
+        answerBottom.setTitle(bottomChoice, for: .normal)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+        
+        if segue.identifier == "EditSegue" {
+            creationController.initialQuestion = questionCard.text
+            creationController.initialAnswer = answerCard.text
+        }
     }
 }
 
